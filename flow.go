550f42ae561afce1e438f1b2cfbdbba7be168e49
yutakakinjyo <yutakakinjyo@gmail.com>
2016-02-11 17:04:42 +0900

    roll print

--- flow.go
+++ flow.go
@@ -7,8 +7,17 @@ import (
 )
 
 func main() {
+	slice := []int{0, 0, 0, 0, 0, 0}
 	rand.Seed(time.Now().UnixNano())
-	for i := 0; i < 100; i++ {
-		fmt.Println(rand.Intn(6) + 1)
+	for i := 0; i < 10000000; i++ {
+		roll := rand.Intn(6) + 1
+		slice[roll-1] += 1
 	}
+
+	fmt.Println("1 :", slice[0])
+	fmt.Println("2 :", slice[1])
+	fmt.Println("3 :", slice[2])
+	fmt.Println("4 :", slice[3])
+	fmt.Println("5 :", slice[4])
+	fmt.Println("6 :", slice[5])
 }
